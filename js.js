098187b0bcdd96f9b2b534ed5cada25bc4b49e84
Ojesh Manandhar <gouenji.suyya.10@gmail.com>
2018-12-10 19:14:23 +0545

    Take I/P ang give O/P in string
    While using numbers "2.00" was displayed as "2"

--- js.js
+++ js.js
@@ -1,23 +1,21 @@
 let flag = 1;
 let flagResult = 0;
-let firstNum = 0;
-let secondNum = 0;
-let result = 0;
+let firstNum = '0';
+let secondNum = '0';
+let result = '0';
 let operator = '=';
 let decimalFlag = 0;
-let decimalPlace = 0;
 
 function CE() {
     console.log("CE()");
 
     flag = 1;
-    flagResult = 0;
-    firstNum = 0;
-    secondNum = 0;
-    result = 0;
+    flagResult = 1;
+    firstNum = '0';
+    secondNum = '0';
+    result = '0';
     operator = '=';
     decimalFlag = 0;
-    decimalPlace = 0;
 
     document.getElementById("display").value = '0';
 }
@@ -28,35 +26,34 @@ function operation(op) {
     if (flag === 2 && secondNum !== 0) {
         //work on old operator
         switch (operator) {
-            case '/':
-                firstNum = firstNum/secondNum;
-                break;
-            case '*':
-                firstNum = firstNum*secondNum;
-                break;
-            case '-':
-                firstNum = firstNum - secondNum;
-                break;
-            case '+':
-                firstNum = firstNum + secondNum;
-                break;
-            }
+        case '/':
+            firstNum = (parseFloat(firstNum)/parseFloat(secondNum)).toString();
+            break;
+        case '*':
+            firstNum = (parseFloat(firstNum)*parseFloat(secondNum)).toString();
+            break;
+        case '-':
+            firstNum = (parseFloat(firstNum) - parseFloat(secondNum)).toString();
+            break;
+        case '+':
+            firstNum = (parseFloat(firstNum) + parseFloat(secondNum)).toString();
+            break;
+        }
         
-        secondNum = 0;
+        secondNum = '0';
     }
 
     operator = op;
 
     if (flagResult === 1) {
         firstNum = result;
-        secondNum = 0;
+        secondNum = '0';
 
         flagResult = 0;
     }
 
     flag = 2;
     decimalFlag = 0;
-    decimalPlace = 0;
 }
 
 function clickedNumber(num) {
@@ -64,30 +61,29 @@ function clickedNumber(num) {
 
     if (flagResult === 1) {
         flagResult = 0;
+        decimalFlag = 0;
     }
 
-    if (decimalFlag === 1 && num === -1) {
-        decimalFlag = 2;
-        num = 0;
-    }
-
-    if (num === -1){
-        num = 0;
+    if (num === '-1'){
         decimalFlag = 1;
-        decimalPlace = 1;
     }
 
     if (flag === 1) {
-        if (firstNum < Math.pow(10, 14)) {
-            if (decimalFlag === 1) {
-                firstNum = firstNum + num/decimalPlace;
-                decimalPlace *= 10;
+        if (firstNum.length < 15) {
+            if (firstNum === '0') {
+                firstNum = '';
             }
-            else if (decimalFlag === 0) {
-                firstNum = firstNum*10 + num
+            
+            if (num === '-1') {
+                if (firstNum === '') {
+                    firstNum += '0.';
+                }
+                else {
+                    firstNum += '.';
+                }
             }
-            else if (decimalFlag === 2) {
-                decimalFlag = 1;
+            else {
+                firstNum += num;
             }
 
             console.log("firstNum = " + firstNum);
@@ -99,16 +95,21 @@ function clickedNumber(num) {
             document.getElementById("display").value = '';
         }
 
-        if (secondNum < Math.pow(10, 14)) {
-            if (decimalFlag === 1) {
-                secondNum = secondNum + num/decimalPlace;
-                decimalPlace *= 10;
+        if (secondNum.length < 15) {
+            if (secondNum === '0') {
+                secondNum = '';
             }
-            else if (decimalFlag === 0) {
-                secondNum = secondNum*10 + num
+            
+            if (num === '-1') {
+                if (secondNum === '') {
+                    secondNum += '0.';
+                }
+                else {
+                    secondNum += '.';
+                }
             }
-            else if (decimalFlag === 2) {
-                decimalFlag = 1;
+            else {
+                secondNum += num;
             }
 
             console.log("secondNum = " + secondNum);
@@ -123,16 +124,16 @@ function calculate() {
 
     switch (operator) {
     case '/':
-        result = firstNum/secondNum;
+        result = (parseFloat(firstNum)/parseFloat(secondNum)).toString();
         break;
     case '*':
-        result = firstNum*secondNum;
+        result = (parseFloat(firstNum)*parseFloat(secondNum)).toString();
         break;
     case '-':
-        result = firstNum - secondNum;
+        result = (parseFloat(firstNum) - parseFloat(secondNum)).toString();
         break;
     case '+':
-        result = firstNum + secondNum;
+        result = (parseFloat(firstNum) + parseFloat(secondNum)).toString();
         break;
     case '=':
         result = firstNum;
